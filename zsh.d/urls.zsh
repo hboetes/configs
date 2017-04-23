09ede3c8b0b9280d8437335ee500a0cfd45c48fc
Han Boetes <hboetes@gmail.com>
2017-04-23 12:10:27 +0200

    FINALLY!!!! I FOUND IT!!!!! simply put quotes around a pasted url!!!! I'm not alone in this world!!!!

--- zsh.d/urls.zsh
+++ zsh.d/urls.zsh
@@ -1,10 +1,2 @@
-# autoload -Uz url-quote-magic  bracketed-paste-magic
-# zstyle ':urlglobber' url-other-schema ftp git gopher http https magnet
-# zstyle ':url-quote-magic:*' url-metas '*?[]^(|)~#='  # dropped { }
-# zle -N self-insert url-quote-magic
-# zle -N bracketed-paste bracketed-paste-magic
-
-
-autoload -Uz url-quote-magic bracketed-paste-magic
-zle -N self-insert url-quote-magic
-zle -N bracketed-paste bracketed-paste-magic
+autoload -Uz bracketed-paste-url-magic
+zle -N bracketed-paste bracketed-paste-url-magic
