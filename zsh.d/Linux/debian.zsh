b428da91cff6ccb277a6308eb9fb4cc8d747afc6
Han Boetes <hboetes@gmail.com>
2017-08-25 21:40:46 +0200

    Various improvements

--- zsh.d/Linux/debian.zsh
+++ zsh.d/Linux/debian.zsh
@@ -7,7 +7,10 @@ if command -v quilt >& /dev/null; then
     alias dquilt="quilt --quiltrc=${HOME}/.quiltrc-dpkg"
 fi
 
-alias packages_by_size='dpkg-query -W --showformat='${Installed-Size;10}\t${Package}\n' | sort -k1,1n'
+packages_by_size()
+{
+    dpkg-query -W --showformat='${Installed-Size;10}\t${Package}\n' | sort -k1,1n
+}
 
 orphaner()
 {
