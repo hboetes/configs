0e3e9bb45fecf3e84f9b139bc70c80128e80a7b3
Han Boetes <hboetes@gmail.com>
2017-10-14 15:35:47 +0200

    - remove all instances of ws-butler, which was too polite - only chmod files which don't match, this saves the amount of syscalls drastically, also for backups

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,8 +30,7 @@
  '(transient-mark-mode 1)
  '(w3m-fill-column 80)
  '(w3m-home-page "http://boetes.org")
- '(w3m-key-binding (quote info))
- '(ws-butler-global-mode t))
+ '(w3m-key-binding (quote info)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
