edb147762c3a4901a8b0f46444ff72b63fea3494
Han Boetes <hboetes@gmail.com>
2017-12-17 19:29:02 +0100

    Fix: https://github.com/glasserc/ethan-wspace/issues/28

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-require-final-newline nil)
  '(blink-cursor-mode nil)
  '(colon-double-space t)
  '(column-number-mode t)
