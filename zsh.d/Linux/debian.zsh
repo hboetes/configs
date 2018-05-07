b753c661fb5b4b8cd8accd2a38b5a2c9ab07f054
Han Boetes <hboetes@gmail.com>
2018-05-07 09:54:27 +0200

    Add support for proxmox kernels

--- zsh.d/Linux/debian.zsh
+++ zsh.d/Linux/debian.zsh
@@ -28,7 +28,15 @@ apt-removerc()
 apt-upgrade()
 {
     # Remove old unused kernels, except for the last one.
-    dpkg -l 'linux-image-4.*'|awk '/^(ii|rc)/ {print $2}' | sort -n -t- -k4 | sed -e "/$(uname -r)/,\$d" | head -n -1 | xargs -r sudo apt-get -y autoremove --purge
+    case $(uname -r) in
+        *pve)
+            SS='pve-kernel-4.*'
+            ;;
+        *)
+            SS='linux-image-4.*'
+            ;;
+    esac
+    dpkg -l "$SS" | awk '/^(ii|rc)/ {print $2}' | sort -n -t- -k4 | sed -e "/$(uname -r)/,\$d" | head -n -1 | xargs -r sudo apt-get -y autoremove --purge
     if command -v apt-metalink >& /dev/null; then
         local aptgetter=apt-metalink
     else
