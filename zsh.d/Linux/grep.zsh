4b131bf25aaa211d9548109ba48ec4b34424a52d
Han Boetes <h.boetes@axis-simulation.com>
2018-05-24 16:58:39 +0200

    get rid of silly notification on vms

--- zsh.d/Linux/grep.zsh
+++ zsh.d/Linux/grep.zsh
@@ -1,5 +1,5 @@
 if command -v ag > /dev/null; then
-    alias ag='ACK_PAGER_COLOR="less -x4SRFX" ag --nonumbers'
+    alias ag='ACK_PAGER_COLOR="less -x4SRFX" ag --nonumbers --noaffinity'
     alias grep=ag
     alias zgrep='ag -z'
 elif command -v ack > /dev/null; then
