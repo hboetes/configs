4dd5fdb157bf64493080787ff94d7ffae3ef3f64
Han Boetes <h.boetes@axis-simulation.com>
2018-06-12 09:44:28 +0200

    if only this would work

--- zsh.d/Linux/grep.zsh
+++ zsh.d/Linux/grep.zsh
@@ -1,10 +1,5 @@
-# Test if we're inside of a container.
-if ! \grep -qv '/$' /proc/1/cgroup; then
-    extra=" --noaffinity"
-fi
-
 if command -v ag > /dev/null; then
-    alias ag="ACK_PAGER_COLOR='less -x4SRFX' ag --nonumbers$extra"
+    alias ag="ACK_PAGER_COLOR='less -x4SRFX' ag --nonumbers --noaffinity"
     alias grep=ag
     alias zgrep='ag -z'
 elif command -v ack > /dev/null; then
