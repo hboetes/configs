3c483a34b933cdebab94cde4f3b14da40fa8fe83
Han Boetes <han@boetes.org>
2019-05-16 21:07:22 +0200

    ag -z is broken

--- zsh.d/DragonFly/grep.zsh
+++ zsh.d/DragonFly/grep.zsh
@@ -1,7 +1,13 @@
 if command -v ag > /dev/null; then
     alias ag='ACK_PAGER_COLOR="w3m" ag --nonumbers'
     alias grep=ag
-    alias zgrep='ag -z'
+    function zgrep() {
+        search=$1
+        shift
+        for i in $*; do
+            zcat $i| ag $search
+        done
+    }
 elif command -v ack > /dev/null; then
     alias ack='ACK_PAGER_COLOR="w3m" ack'
     alias grep=ack
