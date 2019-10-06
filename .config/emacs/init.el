81c33ba7dd1b56cae4c217d40023f09ac707a4bc
Han Boetes <han@boetes.org>
2019-10-06 16:13:10 +0200

    blah

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -15,7 +15,7 @@
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(yaml-mode eterm-256color ample-theme airline-themes flycheck ethan-wspace smart-mode-line-powerline-theme smart-mode-line puppet-mode pager nginx-mode async))
+   '(magit yaml-mode eterm-256color ample-theme airline-themes flycheck ethan-wspace smart-mode-line-powerline-theme smart-mode-line puppet-mode pager nginx-mode async))
  '(safe-local-variable-values
    '((epa-file-cache-passphrase-for-symmetric-encryption . 1)
      (add-log-time-zone-rule . t)))
--- zsh.d/habo01/habo01.zsh
+++
@@ -1,3 +0,0 @@
-alias lgg6sync='TZ=UTC \rsync -vi -aP --modify-window=1 --no-p --no-g --delete-before --no-perms --no-times --size-only /mp3/misc/Android_music/ lgg6:mp3'
-# alias avesync='TZ=UTC \rsync -vi -rlpgoDvcP --modify-window=1 --no-p --no-g --delete-before /kluizenaar/music/misc/Android_music/ /ssd/avensis'
-alias silverbox='xfreerdp /u:h.boetes@win /f /v:silverbox /fonts /sound'
