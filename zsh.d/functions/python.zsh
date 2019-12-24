e5102f9630103ff3a7f38fc2ca905d925f259cb1
Han Boetes <han@boetes.org>
2019-12-24 21:12:36 +0100

    What do you do if you don't have pip, huh!?

--- zsh.d/functions/python.zsh
+++ zsh.d/functions/python.zsh
@@ -1,4 +1,10 @@
 pip-update()
 {
-    pip freeze --local | cut -d = -f 1 | xargs -n1 pip install -U --user
+    if ! test -x ~/.local/bin/pip; then
+        curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+        python3 get-pip.py --user
+        rm get-pip.py
+    else
+        pip list --outdated --format=freeze --user | cut -d = -f 1 | xargs -r -n1 pip install -U --user
+    fi
 }
