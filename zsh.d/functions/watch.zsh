b0a180d476737a0ed9993a4484a1ffd8510d52d8
Han Boetes <hboetes@gmail.com>
2020-05-17 10:05:45 +0200

    improve code, use local and avoid mixing up %

--- zsh.d/functions/watch.zsh
+++ zsh.d/functions/watch.zsh
@@ -1,5 +1,6 @@
 watch () {
-    IN=2
+    local PAD
+    local IN=2
     case $1 in
         -n)
             IN=$2
@@ -7,13 +8,13 @@ watch () {
             ;;
     esac
     printf '\033c'
-    CM="$*"
-    LEFT="$(printf 'Every %.1f: %s' $IN $CM)"
+    local CM="$*"
+    local LEFT="$(printf 'Every %.1f: %s' $IN $CM)"
     ((PAD = COLUMNS - ${#LEFT}))
     while :
     do
-        DT=$(date)
-        printf "$LEFT%${PAD}s\n" "$HOST $(date)"
+        echo -nE "$LEFT"
+        printf "%${PAD}s\n" "$HOST $(date)"
         eval "$CM"
         sleep $IN
         printf '\033c'
