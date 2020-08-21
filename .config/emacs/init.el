2fe8b0ebdd30c61905761cb785d2016e16ee8be8
Han Boetes <hboetes@gmail.com>
2020-08-21 19:27:03 +0000

    Use monokai, nice useable colors

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -17,7 +17,7 @@
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(magit crontab-mode csv-mode smartparens yaml-mode eterm-256color ample-theme airline-themes flycheck ethan-wspace smart-mode-line-powerline-theme smart-mode-line puppet-mode pager nginx-mode async))
+   '(monokai-theme magit crontab-mode csv-mode smartparens yaml-mode eterm-256color ample-theme airline-themes flycheck ethan-wspace smart-mode-line-powerline-theme smart-mode-line puppet-mode pager nginx-mode async))
  '(safe-local-variable-values
    '((epa-file-cache-passphrase-for-symmetric-encryption . 1)
      (add-log-time-zone-rule . t)))
@@ -358,7 +358,7 @@
 ;; Themes and stuff
                                         ; Yes, all themes are safe.
 (setq custom-safe-themes t)
-(load-theme 'ample t)
+(load-theme 'monokai t)
 
 ;; (custom-theme-set-faces
 ;;  'zenburn
