b3ffc3990e0d42e2c2140570270c303984e28bc7
Han Boetes <h.boetes@DOMAIN>
2020-09-25 16:51:14 +0200

    speedtest-cli is cool

--- zsh.d/alias.zsh
+++ zsh.d/alias.zsh
@@ -34,6 +34,7 @@ isinpath mycli   && alias mycli='LANG=C.UTF8 LC_ALL=C.UTF-8 mycli'
 isinpath xclip   && alias xclip_image='xclip -selection clipboard -t image/png -i'
 isinpath bat     && alias cat='bat --tabs=8'
 isinpath batcat  && alias cat='batcat --tabs=8'
+isinpath speedtest-cli  && alias speedtest-cli='speedtest-cli --bytes'
 
 # Global aliases
 alias -g L='|less'
