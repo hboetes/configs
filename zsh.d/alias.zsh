e13b8f2b144cb505a7796e07e162858848b6d786
Han Boetes <han@DOMAIN>
2020-11-30 00:17:38 +0100

    wrap long lines

--- zsh.d/alias.zsh
+++ zsh.d/alias.zsh
@@ -32,8 +32,8 @@ isinpath pwgen  && alias pwgen='pwgen -y 12 1'
 isinpath objdump && alias which_compiler='objdump --full-contents --section=.comment'
 isinpath mycli   && alias mycli='LANG=C.UTF8 LC_ALL=C.UTF-8 mycli'
 isinpath xclip   && alias xclip_image='xclip -selection clipboard -t image/png -i'
-isinpath bat     && alias cat='bat --tabs=8'
-isinpath batcat  && alias cat='batcat --tabs=8'
+isinpath bat     && alias cat='bat --tabs=8 --wrap character'
+isinpath batcat  && alias cat='batcat --tabs=8 --wrap character'
 isinpath speedtest-cli  && alias speedtest-cli='speedtest-cli --bytes'
 
 # Global aliases
