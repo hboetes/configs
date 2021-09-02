bb3bda30c399c44294511354cb16c523de9cf2c6
Han Boetes <hboetes@gmail.com>
2021-09-02 18:50:26 +0200

    w3m is not so great as a pager

--- zsh.d/w3m.zsh
+++ zsh.d/w3m.zsh
@@ -1,7 +1,5 @@
 # set w3m as the pager and manpager if it exists
 [[ -e /etc/redhat-release ]] && return 0
 isinpath w3m || return 0
-export PAGER=w3m
-alias less='w3m'
-alias man='env COLUMNS=80 w3mman'
+alias man='env PAGER=w3m COLUMNS=80 w3mman'
 compdef w3mman=man
