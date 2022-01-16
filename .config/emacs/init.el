8430f0775ca6d4d7353b8aa3c830952fcf9bc5a9
Han Boetes <han@boetes.org>
2022-01-16 05:07:43 +0100

    whitespace

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -38,6 +38,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "monofur for Powerline" :foundry "unci" :slant normal :weight normal :height 158 :width normal)))))
+
 ;; Fix a few bugs.
 (add-hook 'term-mode-hook #'eterm-256color-mode)
 
