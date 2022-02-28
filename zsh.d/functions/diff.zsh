20da12c4f3770d2ccdc86e031cf25f94bab91bbd
Han Boetes <hboetes@gmail.com>
2022-03-01 00:18:18 +0100

    first time in my life I got spammed by a program

--- zsh.d/functions/diff.zsh
+++ zsh.d/functions/diff.zsh
@@ -1,14 +1,3 @@
-if isinpath delta; then
-    diff()
-    {
-        delta $*
-        return 0
-    }
-    export GIT_PAGER=delta
-    export BAT_PAGER=less
-    export BAT_THEME=base16
-fi
-
 isinpath colordiff || return 0
 diff()
 {
