68d04f0f8053136fa6bb28cdc2d9c79e85f2d279
Han Boetes <h.boetes@axis-flight-training-systems.at>
2022-06-03 13:07:09 +0200

    nice wrapper around man

--- zsh.d/functions/man.zsh
+++ zsh.d/functions/man.zsh
@@ -1,9 +1,11 @@
 [[ $TERM != xterm-256color ]] && return
 man() {
-    LESS_TERMCAP_md=$'\e[1;36m' \
-                   LESS_TERMCAP_me=$'\e[0m' \
-                   LESS_TERMCAP_us=$'\e[1;32m' \
-                   LESS_TERMCAP_ue=$'\e[0m' \
-                   GROFF_NO_SGR=1 \
-                   command man "$@"
+    (($COLUMNS > 80)) && COLUMNS=80
+    COLUMNS=$COLUMNS \
+        LESS_TERMCAP_md=$'\e[1;36m' \
+        LESS_TERMCAP_me=$'\e[0m' \
+        LESS_TERMCAP_us=$'\e[1;32m' \
+        LESS_TERMCAP_ue=$'\e[0m' \
+        GROFF_NO_SGR=1 \
+        command man "$@"
 }
--- zsh.d/w3m.zsh
+++
@@ -1,5 +0,0 @@
-# set w3m as the pager and manpager if it exists
-[[ -e /etc/redhat-release ]] && return 0
-isinpath w3m || return 0
-alias man='env PAGER=w3m COLUMNS=80 w3mman'
-compdef w3mman=man
