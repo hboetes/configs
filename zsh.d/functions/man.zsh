a6b2e3d6454303162465e2bd3ceca6592827034e
Han Boetes <han@boetes.org>
2022-06-12 01:27:34 +0200

    w3mman strikes back

--- zsh.d/functions/man.zsh
+++ zsh.d/functions/man.zsh
@@ -1,11 +1,16 @@
-[[ $TERM != *-256color ]] && return
-man() {
-    (($COLUMNS > 80)) && COLUMNS=80
-    COLUMNS=$COLUMNS \
-        LESS_TERMCAP_md=$'\e[1;36m' \
-        LESS_TERMCAP_me=$'\e[0m' \
-        LESS_TERMCAP_us=$'\e[1;32m' \
-        LESS_TERMCAP_ue=$'\e[0m' \
-        GROFF_NO_SGR=1 \
-        command man "$@"
-}
+if isinpath w3mman; then
+    man() {
+        env COLUMNS=80 w3mman "$@"
+    }
+else
+    man() {
+        (($COLUMNS > 80)) && COLUMNS=80
+        COLUMNS=$COLUMNS \
+            LESS_TERMCAP_md=$'\e[1;36m' \
+            LESS_TERMCAP_me=$'\e[0m' \
+            LESS_TERMCAP_us=$'\e[1;32m' \
+            LESS_TERMCAP_ue=$'\e[0m' \
+            GROFF_NO_SGR=1 \
+            command man "$@"
+    }
+fi
