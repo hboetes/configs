5907e1c65ecddd1d2156fa2a7fe56dd49de09880
Han Boetes <h.boetes@axis-flight-training-systems.at>
2023-10-06 12:38:31 +0200

    How cool is that, everything in one command

---
+++ .config/zsh.d/functions/genssl.zsh
@@ -0,0 +1,13 @@
+gensslcsr()
+{
+    if [ -z "$1" ]; then
+        echo "This function requires an argument: A name. :P" >&2
+    else
+        (
+            umask 077
+            openssl req -nodes -newkey rsa:2048 -keyout $1.axis-flight-training-systems.at.key -out $1.axis-flight-training-systems.at.csr -subj "/C=AT/ST=Steiermark/L=Lebring/O=Axis Flight Training Systems GmbH/OU=IT/CN=$1.axis-flight-training-systems.at"
+            echo "Principal alias: host/$fqdn@AXIS-FLIGHT-TRAINING-SYSTEMS.AT"
+            cat $fqdn.csr
+        )
+    fi
+}
