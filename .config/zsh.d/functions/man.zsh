2da792ced599e23cc1ffe3173293e2f148e7e367
Han <han@bran>
2025-11-14 19:01:05 +0100

    I have so had it with w3mman

--- .config/zsh.d/functions/man.zsh
+++ .config/zsh.d/functions/man.zsh
@@ -1,18 +1,10 @@
-if isinpath w3mman; then
-    man() {
-        (($COLUMNS > 80)) && COLUMNS=80
-        env COLUMNS=$COLUMNS \
-            w3mman "$@"
-    }
-else
-    man() {
-        (($COLUMNS > 80)) && COLUMNS=80
-        COLUMNS=$COLUMNS \
-            LESS_TERMCAP_md=$'\e[1;36m' \
-            LESS_TERMCAP_me=$'\e[0m' \
-            LESS_TERMCAP_us=$'\e[1;32m' \
-            LESS_TERMCAP_ue=$'\e[0m' \
-            GROFF_NO_SGR=1 \
-            command man "$@"
-    }
-fi
+man() {
+    (($COLUMNS > 80)) && COLUMNS=80
+    COLUMNS=$COLUMNS \
+        LESS_TERMCAP_md=$'\e[1;36m' \
+        LESS_TERMCAP_me=$'\e[0m' \
+        LESS_TERMCAP_us=$'\e[1;32m' \
+        LESS_TERMCAP_ue=$'\e[0m' \
+        GROFF_NO_SGR=1 \
+        command man "$@"
+}
