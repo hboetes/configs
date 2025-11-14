5444c0e168fd85250d8b91a48f5622365821570d
Han Boetes <hboetes@gmail.com>
2025-11-14 19:03:40 +0100

    trash

--- .config/hypr/scripts/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Environment
-systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
